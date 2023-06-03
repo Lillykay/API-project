8ce001f2919f8229d3079660a0ee3219f7e82a10
Lillycladwell <lillycladwell@email.com>
2023-06-02 21:07:37 -0500

    phase1 authenticate me frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./index.css";
+
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+
+import App from "./App";
+import configureStore from "./store";
+import * as sessionActions from "./store/session";
+
+const store = configureStore();
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
